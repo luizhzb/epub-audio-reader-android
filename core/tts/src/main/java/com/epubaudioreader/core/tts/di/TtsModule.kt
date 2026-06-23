ae395465cf0fdabb19dd651b3bbd460f6cba2073
luizhzb <luizhenriquezanoti@gmail.com>
2026-06-23 09:46:37 -0300

    refactor: TtsModule Hilt atualizado

--- core/tts/src/main/java/com/epubaudioreader/core/tts/di/TtsModule.kt
+++ core/tts/src/main/java/com/epubaudioreader/core/tts/di/TtsModule.kt
@@ -2,7 +2,6 @@ package com.epubaudioreader.core.tts.di
 
 import com.epubaudioreader.core.tts.engine.SherpaOnnxTtsEngine
 import com.epubaudioreader.core.tts.engine.TtsEngine
-import com.epubaudioreader.core.tts.player.EpubTtsPlayer
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,5 +14,4 @@ abstract class TtsModule {
     @Binds
     @Singleton
     abstract fun bindTtsEngine(impl: SherpaOnnxTtsEngine): TtsEngine
-    // EpubTtsPlayer uses @Inject constructor, no binding needed
 }
